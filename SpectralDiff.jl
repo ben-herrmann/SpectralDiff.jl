0d4f3d9d013e00cc67d564141f5cc8bee50c4f3c
ben-herrmann <benherrm@uw.edu>
2020-08-25 11:10:00 -0700

    Added Hermite polynomials

--- SpectralDiff.jl
+++ SpectralDiff.jl
@@ -1,12 +1,13 @@
 module SpectralDiff
 
-export fourier_grid, fourier_diff, cheb_grid, cheb_diff, cheb_weights
+export fourier_grid, fourier_diff, fourier_weights
+export cheb_grid, cheb_diff, cheb_weights
+export her_grid, her_diff, her_weights
 
 using LinearAlgebra
-using ToeplitzMatrices
 
 function fourier_grid(N::Integer)
-    x = (0:N-1)*2π/N
+    x = collect(0:N-1)*2π/N
     return x
 end
 
@@ -35,6 +36,11 @@ function fourier_diff(N::Integer,degree::Integer=1)
         D = D2^div(degree,2)*D1^(degree%2)
     end
     return D
+end
+
+function fourier_weights(N::Integer)
+    w = 2π/N*ones(N)
+    return w
 end
 
 function cheb_grid(N::Integer)
@@ -75,4 +81,48 @@ function cheb_weights(N::Integer)
     return w
 end
 
+function her_grid(N::Integer,b::Real=1.0)
+    #  J.A.C. Weideman, S.C. Reddy 1998.
+    J = SymTridiagonal(zeros(N),sqrt.(1:N-1))
+    r = eigvals(J)/sqrt(2)
+    x = r/b
+    return x
+end
+
+function her_diff(N::Integer,b::Real=1.0,degree::Integer=1.0)
+    #  J.A.C. Weideman, S.C. Reddy 1998.
+    x = her_grid(N)
+    α = exp.(-x.^2/2)
+    β = Array{typeof(x[1])}(undef,degree+1,N)
+    β[1,:] = ones(length(x))
+    β[2,:] = -x
+    for l=3:degree+1
+        β[l,:] = -x.*β[l-1,:].-(l-2)*β[l-2,:]
+    end
+    β = β[2:end,:]
+    XX = repeat(x,1,N)
+    DX = XX-XX'
+    DX[I(N)].=1.0
+    c = α.*prod(DX,dims=2)
+    C = repeat(c,1,N)
+    C = C./C'
+    Z = 1 ./DX
+    Z[I(N)].=0.0
+    X = Z'
+    X = reshape(X[.~I(N)],N-1,N)
+    Y = ones(N-1,N)
+    D = I(N)
+    for d=1:degree
+        Y = cumsum([β[d,:]'; d*Y[1:N-1,:].*X],dims=1)
+        D = d*Z.*(C.*repeat(diag(D),1,N)-D)
+        D[I(N)]=Y[N,:]
+    end
+    D = b^degree*D
+    return D
+end
+
+function her_weights(N::Integer,b::Real=1.0)
+    x = her_grid(N,b)
+    w = ([diff(x);0]+[0;diff(x)])/2
+    return w
 end
